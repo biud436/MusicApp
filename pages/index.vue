61332e99e0e66feff59bf1ac882149add6d3e985
Jinseok Eo <biud436@gmail.com>
2023-03-28 16:54:19 +0900

    fix: 컴포지션 API로 변경

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,49 @@
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia';
+import IncrementalContent from '@/components/IncrementalContent.vue'
+import Counter from '@/components/Counter.vue';
+import User from '@/components/User.vue';
+import { useCounterStore } from '~~/composables/counter';
+import { useServerStore } from '~~/composables/server';
+
+const data = ref({});
+const isShake = ref(false);
+const counterStore = useCounterStore();
+const serverStore = useServerStore()
+
+const { counter } = storeToRefs(counterStore);
+const targetValue = ref(100);
+
+const increment = () => {
+    counterStore.increment();
+    startShake();
+}
+
+const decrement = () => {
+    counterStore.decrement();
+    startShake();
+}
+
+const startShake = () => {
+    isShake.value = true;
+
+    setTimeout(() => {
+        isShake.value = false;
+    }, 300);
+}
+
+onMounted(async () => {
+    data.value = await serverStore.getData();
+});
+
+watch(targetValue, (newVal, oldVal) => {
+    const diff = Math.abs(newVal - counter.value);
+    if (diff < 10) {
+        startShake();
+    }
+});
+
+</script>
 <template>
     <main>
         <div class="container mx-auto">
@@ -19,61 +65,3 @@
         </div>
     </main>
 </template>
-<script lang="ts">
-import { storeToRefs } from 'pinia';
-import IncrementalHeader from '@/components/IncrementalHeader.vue'
-import IncrementalContent from '@/components/IncrementalContent.vue'
-import Counter from '@/components/Counter.vue';
-import User from '@/components/User.vue';
-import { useCounterStore } from '~~/composables/counter';
-import { useServerStore } from '~~/composables/server';
-
-export default defineComponent({
-    setup() {
-        const data = ref({});
-        const isShake = ref(false);
-        const counterStore = useCounterStore();
-        const serverStore = useServerStore()
-
-        const { counter } = storeToRefs(counterStore);
-        const targetValue = ref(100);
-
-        return {
-            serverStore: serverStore,
-            counterStore,
-            counter,
-            data,
-            isShake,
-            targetValue
-        };
-    },
-    async mounted() {
-        this.data = await this.serverStore.getData();
-    },
-    watch: {
-        targetValue: {
-            handler(newVal, oldVal) {
-                const diff = Math.abs(newVal - this.counter);
-            },
-        }
-    },
-    methods: {
-        increment() {
-            this.counterStore.increment();
-            this.startShake();
-        },
-        decrement() {
-            this.counterStore.decrement();
-            this.startShake();
-        },
-        startShake() {
-            this.isShake = true;
-
-            setTimeout(() => {
-                this.isShake = false;
-            }, 300);
-        },
-    },
-    components: { IncrementalHeader, IncrementalContent, Counter, User }
-})
-</script>
