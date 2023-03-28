a093c1c5bdb4479b1fe6000eee87c23d0682656d
Jinseok Eo <biud436@gmail.com>
2023-03-28 14:03:08 +0900

    feat: added ma-2 and pa-2 from vuetify

---
+++ components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card-1">
+        <h3>카운터 체크</h3>
+        <input type="number" class="input-form" v-model="targetValue" />
+    </div>
+</template>
+<script lang="ts">
+const Counter = defineComponent({
+    props: {
+        targetValue: {
+            type: Number,
+            default: 100
+        }
+    },
+});
+
+export default Counter;
+</script>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
     <main>
         <div class="container mx-auto">
-            <div class="columns-2 gap-3 flex flex-col bg-gray-100 rounded-sm ma-2 pa-2 justify-items-center items-center">
+            <div class="card-1">
                 <div>
                     <h2 class="text-2xl hover:text-blue-600">
                         Counter Store
@@ -14,15 +14,16 @@
                     <IncrementalContent :increment="increment" :decrement="decrement" />
                 </div>
             </div>
-            <div class="columns-2 flex flex-col bg-gray-100 rounded-sm ma-2 pa-2 gap-3" style="flex-direction: column;">
-                <div class="text-2xl ">
+            <div class="card-1">
+                <div class=" text-2xl ">
                     <h2>Main Store </h2>
-                    <p class="text-gray-500 text-sm"> (https://jsonplaceholder.typicode.com/todos/1 호출) </p>
+                    <p class=" text-gray-500 text-sm"> (https://jsonplaceholder.typicode.com/todos/1 호출) </p>
                 </div>
                 <div class="bg-slate-800 text-white pa-2 rounded-md">
                     <pre>{{ data }}</pre>
                 </div>
             </div>
+            <Counter :targetValue="targetValue" />
         </div>
     </main>
 </template>
@@ -31,8 +32,8 @@ import { storeToRefs } from 'pinia';
 import IncrementalHeader from '@/components/IncrementalHeader.vue'
 import IncrementalContent from '@/components/IncrementalContent.vue'
 import { useCounterStore } from '~~/composables/counter';
+import Counter from '@/components/Counter.vue';
 
-// Vue 2 문법을 사용한다
 export default defineComponent({
     setup() {
         const data = ref({});
@@ -41,17 +42,30 @@ export default defineComponent({
         const counterStore = useCounterStore();
 
         const { counter } = storeToRefs(counterStore);
+        const targetValue = ref(100);
+
+        watch(counter, (newVal, oldVal) => {
+
+        });
 
         return {
             mainStore: main,
             counterStore,
             counter,
             data,
-            isShake
+            isShake,
+            targetValue
         };
     },
     async mounted() {
         this.data = await this.mainStore.getData();
+    },
+    watch: {
+        targetValue: {
+            handler(newVal, oldVal) {
+                const diff = Math.abs(newVal - this.counter);
+            },
+        }
     },
     methods: {
         increment() {
@@ -70,7 +84,7 @@ export default defineComponent({
             }, 300);
         },
     },
-    components: { IncrementalHeader, IncrementalContent }
+    components: { IncrementalHeader, IncrementalContent, Counter }
 })
 </script>
 <style lang="scss"></style>
