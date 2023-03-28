ee4269207c00a8321e143661ba9958d8fd67f772
Jinseok Eo <biud436@gmail.com>
2023-03-28 10:35:30 +0900

    refactor: 카운터, 메인 스토어 분리

--- composables/index.ts
+++ composables/index.ts
@@ -1,21 +1,16 @@
 import { defineStore } from 'pinia';
 
-export type TodoData = {userId: number; id: number; title: string; completed: boolean;}
+export type TodoData = {
+    userId: number; 
+    id: number; 
+    title: string; 
+    completed: boolean;
+};
 
 export const useMainStore = defineStore("main", {
     state: () => ({
-        counter: 0,
     }),
     actions: {
-        reset() {
-            this.counter = 0;
-        },
-        increment() {
-            this.counter++;
-        },
-        decrement() {
-            this.counter--;
-        },
         async getData(): Promise<TodoData> {
             const data = await $fetch<TodoData>('https://jsonplaceholder.typicode.com/todos/1');
             return data;
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,31 @@
 <template>
     <main>
         <v-container gutter="24">
-            <v-row dense>
-                <IncrementalHeader :counter="counter">
+            <v-row dense class="gap-4" style="flex-direction: column;">
+                <v-col>
+                    <h2>
+                        Counter Store
+                    </h2>
+                </v-col>
+                <!-- <IncrementalHeader :counter="counter">
                     <ContentDoc />
-                </IncrementalHeader>
-                <IncrementalContent :increment="increment" :decrement="decrement" />
+                </IncrementalHeader> -->
+                <v-col>
+                    {{ counter }}
+                </v-col>
+                <v-col>
+                    <IncrementalContent :increment="increment" :decrement="decrement" />
+                </v-col>
+
             </v-row>
-            <pre>{{ data }}</pre>
+            <v-row class="gap-2" style="flex-direction: column;">
+                <v-col>
+                    <h2>Main Store (https://jsonplaceholder.typicode.com/todos/1 호출) </h2>
+                </v-col>
+                <v-col>
+                    <pre>{{ data }}</pre>
+                </v-col>
+            </v-row>
         </v-container>
     </main>
 </template>
@@ -15,30 +33,40 @@
 import { storeToRefs } from 'pinia';
 import IncrementalHeader from '@/components/IncrementalHeader.vue'
 import IncrementalContent from '@/components/IncrementalContent.vue'
+import { useCounterStore } from '~~/composables/counter';
 
 // Vue 2 문법을 사용한다
 export default defineComponent({
     setup() {
         const data = ref({});
         const main = useMainStore();
-        const { counter } = storeToRefs(main);
+        const counterStore = useCounterStore();
+
+        const { counter } = storeToRefs(counterStore);
+
         return {
-            main,
+            mainStore: main,
+            counterStore,
             counter,
             data
         };
     },
     async mounted() {
-        this.data = await this.main.getData();
+        this.data = await this.mainStore.getData();
     },
     methods: {
         increment() {
-            this.main.increment();
+            this.counterStore.increment();
         },
         decrement() {
-            this.main.decrement();
+            this.counterStore.decrement();
         },
     },
     components: { IncrementalHeader, IncrementalContent }
 })
 </script>
+<style lang="scss">
+/* .gap-2 {
+    gap: 2rem;
+} */
+</style>
