da79b98fd51431b063bcf0f5b7393d9c5aaafae3
biud436 <biud436@gmail.com>
2023-04-01 14:35:17 +0900

    fix: changed z-index

--- components/player/PlayerContainer.vue
+++ components/player/PlayerContainer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fixed bottom-0 left-0 w-full h-20 bg-[#191919] min-h-min text-white">
+    <div class="fixed bottom-0 left-0 w-full h-20 bg-[#191919] min-h-min text-white z-40">
         <div class="z-40 flex flex-row h-full gap-2">
             <PlayerInformationPanel>
                 <PlayerAlbum />
