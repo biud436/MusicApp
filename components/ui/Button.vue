d318adbf1162516ceaf75f98a24ad2047ba1ca28
Jinseok Eo <biud436@gmail.com>
2023-04-06 16:46:44 +0900

    refactor: check

---
+++ components/ui/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface ButtonProps {
+    variant: "text" | "outlined" | "contained";
+    text: string;
+}
+
+const { text, variant } = defineProps<ButtonProps>();
+
+</script>
+<template>
+    <button class="relative p-2 transition rounded-md" :class="{
+        'bg-blue-500 text-white hover:bg-blue-600 hover:shadow-sm active:bg-blue-700': variant === 'contained',
+        'border-blue-500 text-blue-500 border-1 hover:shadow-sm': variant === 'outlined',
+        'text-blue-500 hover:text-blue-600 active:text-blue-700 ': variant === 'text'
+    }">
+        <span class="z-10"> {{ text }} </span>
+        <span
+            class="z-0 absolute top-0 left-0 w-full h-full transition scale-[10%] bg-blue-500 rounded-md active:scale-100 overflow-clip"></span>
+    </button>
+</template>
+<style lang="scss"></style>
---
+++ pages/ui/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex flex-col gap-2 p-2 bg-gray-50 h-[100vh]">
+        <UiButton variant="contained" text="Hello World" />
+        <UiButton variant="text" text="Hello World" />
+        <UiButton variant="outlined" text="Hello World" />
+    </div>
+</template>
